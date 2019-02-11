c07e97ad4c8cb90795aa49845185ed3ee2f7f092
alabax <537715+msugakov@users.noreply.github.com>
2019-02-11 21:44:23 +0100

    Using standalone squid With squid cache: 1) 4:13.95elapsed 2) 4:17.05elapsed
    Without squid cache:
    4:48.05elapsed

--- detect-apt-proxy.sh
+++ detect-apt-proxy.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# Detects and configures APT proxy which should run on host or in another container.
+# See Dockerfile for more info.
+
 set -o errexit
 set -o nounset
 set -o pipefail
 
 APT_PROXY_PORT=$1
 HOST_IP=$(route -n | awk '/^0.0.0.0/ {print $2}')
-nc -z "$HOST_IP" ${APT_PROXY_PORT}
 
-if [ $? -eq 0 ]; then
+if nc -z "$HOST_IP" ${APT_PROXY_PORT}; then
+    # TODO: add more exclusions below if you see errors during packages fetching or apt-get update.
+    # Acquire::http::Proxy::ppa.launchpad.net DIRECT;
+    # Acquire::http::Proxy::deb.nodesource.com DIRECT;
+    # Acquire::http::Proxy::packages.microsoft.com DIRECT;
+    # Acquire::http::Proxy::dl.yarnpkg.com DIRECT;
+    # Acquire::https::proxy "https://$HOST_IP:$APT_PROXY_PORT";
     cat >> /etc/apt/apt.conf.d/30proxy <<EOL
     Acquire::http::Proxy "http://$HOST_IP:$APT_PROXY_PORT";
-    Acquire::http::Proxy::ppa.launchpad.net DIRECT;
-    Acquire::http::Proxy::deb.nodesource.com DIRECT;
-    Acquire::http::Proxy::packages.microsoft.com DIRECT;
-    Acquire::http::Proxy::dl.yarnpkg.com DIRECT;
+    Acquire::ftp::proxy "ftp://$HOST_IP:$APT_PROXY_PORT";
 EOL
     cat /etc/apt/apt.conf.d/30proxy
     echo "Using host's apt proxy"
